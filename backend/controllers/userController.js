572284ce1ad8a5ee619a141832054f6756391247
Aditya <goyaladitya203@gmail.com>
2026-06-18 07:19:27 +0530

    SchemaModel.js and few updates

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -120,4 +120,27 @@ const googleLogin = asyncHandler(async (req, res) => {
         }
      });
 
-     export { registerUser, loginUser, googleLogin, getUserProfile };
+
+     const updateUserProfile = asyncHandler(async (req, res) => {
+        const user = await User.findById(req.user._id);
+        if (user) {
+            user.name = req.body.name || user.name;
+            user.email = req.body.email || user.email;
+            user.preferredRole = req.body.preferredRole || user.preferredRole;
+
+            if(req.body.password) {
+                user.password = req.body.password;
+            }
+            
+            const updatedUser = await user.save();
+            res.json({
+                _id: updatedUser._id,
+                name: updatedUser.name,
+                email: updatedUser.email,
+                preferredRole: updatedUser.preferredRole
+            });
+        } else {
+            res.status(404).json({ message: "User not found" });
+        }
+     });
+     export { registerUser, loginUser, googleLogin, getUserProfile, updateUserProfile };
